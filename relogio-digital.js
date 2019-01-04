ea6111316734f903c8122bece66c680395a692d0
Sydrack Gregório <drasyck1@gmail.com>
2019-01-04 17:30:32 -0200

    pronto

--- relogio-digital.js
+++ relogio-digital.js
@@ -1,1 +1,23 @@
-const relogio = ()=>{};
+const relogio = ()=>{
+    var data = new Date();
+    var hrs = data.getHours();
+    var min = data.getMinutes();
+    var seg = data.getSeconds();
+
+    if (hrs < 10) {
+        hrs = "0"+hrs;
+    }
+    if (min < 10) {
+        min = "0"+min;
+    }
+    if (seg < 10) {
+        seg = "0"+seg;
+    }
+
+    var horaF = hora_formatada(hrs, min, seg);
+    document.getElementById("rel").value = horaF;
+};
+
+const hora_formatada = (hrs, min, seg) => hrs+":"+min+":"+seg;
+
+const tempo = setInterval(relogio, 1000);
